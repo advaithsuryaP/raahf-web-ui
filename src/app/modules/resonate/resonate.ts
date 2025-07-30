a24e3c0fadbca02ead96b66603ba761ff3fb1431
advaithsuryaP <padvaithsurya@gmail.com>
2025-07-30 03:23:09 -0400

    Enhance Resonate module with dynamic month and day display
    - Updated resonate.html to replace static rectangles with dynamic month and day elements.
    - Implemented OnInit lifecycle hook in resonate.ts to initialize current month and day, and generate an array of days for the current month.
    - Improved styling with Tailwind CSS for better layout and responsiveness.

--- src/app/modules/resonate/resonate.ts
+++ src/app/modules/resonate/resonate.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-resonate',
@@ -6,4 +6,31 @@ import { Component } from '@angular/core';
     templateUrl: './resonate.html',
     styleUrl: './resonate.css'
 })
-export default class Resonate {}
+export default class Resonate implements OnInit {
+    months: string[] = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    currentMonth: string = '';
+    currentDay: number = 0;
+    days: number[] = [];
+
+    ngOnInit(): void {
+        this.currentMonth = this.months[new Date().getMonth()];
+        const noOfDays = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
+        this.days = Array.from({ length: noOfDays }, (_, i) => i + 1);
+
+        this.currentDay = new Date().getDate();
+    }
+}
